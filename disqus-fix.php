99324e327a8abce5913087008068424137d04522
RichardWatterson <87286040+RichardWatterson@users.noreply.github.com>
2021-08-13 19:09:48 -0500

    Update disqus-fix.php

--- disqus-fix.php
+++ disqus-fix.php
@@ -1,1 +1,29 @@
-<?php /** * Plugin Name: Disqus Comments Preview Fix for TagDiv * Version: 1.1 * Plugin URI: http://farowp.com/ * Description: Este plugin resulve los problemas de los comentarios en modo vista previa para algunos temas como Newspaper y Newsmag. * Author: Richard Watterson * Author URI: http://farowp.com/ * Requires at least: 5.0 * Tested up to: 5.6.2 * * Text Domain: disqus-fix * Domain Path: / * * @package WordPress * @author Richard Watterson * @since 1.1 */ if ( ! defined( 'ABSPATH' ) ) { exit; } add_filter( 'comments_template', function ( $template) { if (is_preview() || !is_file($template)){ return; } return $template; },999);
+<?php
+/**
+ * Plugin Name: Disqus Fix
+ * Version: 1.1
+ * Plugin URI: http://farowp.com/
+ * Description: Este plugin resulve los problemas de los comentarios en modo vista previa para algunos temas como NewsPaper y NewsMag.
+ * Author: Richard Watterson
+ * Author URI: https://farowp.com/
+ * Requires at least: 5.0
+ * Tested up to: 5.6.2
+ *
+ * Text Domain: disqus-fix
+ * Domain Path: /
+ *
+ * @package WordPress
+ * @author Richard Watterson
+ * @since 1.1
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+add_filter( 'comments_template', function ( $template) {	
+	if (is_preview() || !is_file($template)){
+		return;
+	}
+	return $template;
+},999);
